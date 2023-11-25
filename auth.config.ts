6f47b11ba55ad2e91e3526e7e7955bd5098c81fe
philosophynote <adverdest@gmail.com>
2023-11-25 09:21:22 +0900

    secret設定追加

--- auth.config.ts
+++ auth.config.ts
@@ -4,6 +4,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
